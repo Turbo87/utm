8e004d3baef5136ab9cc050874314de537bfe889
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-01-10 20:05:43 +0100

    v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='utm',
-    version='0.3.0',
+    version='0.3.1',
     author='Tobias Bieniek',
     author_email='Tobias.Bieniek@gmx.de',
     url='https://github.com/Turbo87/utm',
