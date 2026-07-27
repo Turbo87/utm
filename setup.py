8fe7f3e7c550c755bb9ab2439028ae0a3e501773
Bart van Andel <bavanandel@gmail.com>
2026-07-27 16:16:12 +0200

    feat: update supported Python versions (#160)
    - Add support for Python 3.14
    - Update dependencies accordingly
    
    BREAKING:
    
    - Drop support for Python 3.9 and below

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         'Topic :: Scientific/Engineering :: GIS',
     ],
     packages=['utm'],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     scripts=['scripts/utm-converter'],
 )
